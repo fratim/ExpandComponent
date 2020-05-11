a5a663250240bf1b524474aed72e1bbb11736777
fratim <43808949+fratim@users.noreply.github.com>
2020-05-11 15:18:35 +0200

    removed unnecessary code

--- dataIO.py
+++ dataIO.py
@@ -5,9 +5,6 @@ import struct
 
 import numpy as np
 from numba import njit
-#from PIL import Image
-
-
 
 import meta_data
 
@@ -64,52 +61,6 @@ def WriteH5File(data, filename, dataset):
     with h5py.File(filename, 'w') as hf:
         # should cover all cases of affinities/images
         hf.create_dataset(dataset, data=data, compression='gzip')
-
-def ReadPoints(prefix, label, dataset):
-    # get the filename for the segmentation
-    point_cloud_filename = '{}/{}/{:06d}.pts'.format(dataset, prefix, label)
-    prefix_zres, prefix_yres, prefix_xres = GridSize(prefix)
-
-    with open(point_cloud_filename, 'rb') as fd:
-        zres, yres, xres, npoints, = struct.unpack('qqqq', fd.read(32))
-        assert (zres == prefix_zres)
-        assert (yres == prefix_yres)
-        assert (xres == prefix_xres)
-        point_cloud = struct.unpack('%sq' % npoints, fd.read(8 * npoints))
-
-    return point_cloud
-
-def ReadAllPoints(prefix, dataset):
-    labels = [int(label[:-4]) for label in sorted(os.listdir('{}/{}'.format(dataset, prefix)))]
-
-    point_clouds = {}
-
-    # read all individual point clouds
-    for label in labels:
-        point_clouds[label] = ReadPoints(prefix, label, dataset)
-
-    return point_clouds
-
-def ReadWidths(prefix, label):
-    # get the filename with all of the widths
-    width_filename = 'widths/{}/{:06d}.pts'.format(prefix, label)
-
-    prefix_zres, prefix_yres, prefix_xres = GridSize(prefix)
-
-    widths = {}
-
-    with open(width_filename, 'rb') as fd:
-        zres, yres, xres, nelements, = struct.unpack('qqqq', fd.read(32))
-        assert (zres == prefix_zres)
-        assert (yres == prefix_yres)
-        assert (xres == prefix_xres)
-
-        for _ in range(nelements):
-            index, width, = struct.unpack('qf', fd.read(12))
-            widths[index] = width
-
-    # return the dictionary of widths for each skeleton point
-    return widths
 
 @njit
 def getPointList(labels_in, blocksize, volumesize,  bz, by, bx, query_ID, dsp_factor):
